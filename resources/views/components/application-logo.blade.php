651ffc0f28d93540f6d1006c3a8188840efef1db
Dirge <fuaddabbad@gmail.com>
2026-03-19 16:29:32 +0700

    refactor | welcome view and update routing

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center gap-3">
+    <img src="{{ asset('logo.svg') }}" alt="Logo" class="w-16" />
+    <h1 class="text-2xl font-bold text-gray-800 dark:text-white">
+        Project library VI
+    </h1>
+</div>
