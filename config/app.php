dd4cdad725e9b2f871bd8ec8a1c3530d1db77d73
Dirga36 <155790299+Dirga36@users.noreply.github.com>
2026-03-25 06:42:38 +0000

    Apply automatic changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', "Project Library VI"),
+    'name' => env('APP_NAME', 'Project Library VI'),
 
     /*
     |--------------------------------------------------------------------------
